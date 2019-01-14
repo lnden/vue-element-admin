6bffc6954ee451bd941bb189534077522891759c
dinglindong <258231674@qq.com>
2019-01-14 18:23:33 +0800

    add svg

---
+++ src/components/SvgIcon/index.vue
@@ -0,0 +1,43 @@
+<template>
+    <svg :class="svgClass" aria-hidden="true">
+        <use :xlink:href="iconName"></use>
+    </svg>
+</template>
+
+<script>
+    export default {
+        name:'svg-cion',
+        props:{
+            iconClass:{
+                type:String,
+                requires:true
+            },
+            className:{
+                type:String
+            }
+        },
+        computed:{
+            iconName(){
+                return `#icon-${this.iconClass}`
+            },
+            svgClass(){
+                if(this.className){
+                    return 'svg-icon'+this.className
+                }else{
+                    return 'svg-icon'
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .svg-icon {
+        width: 20px;
+        height: 20px;
+        vertical-align: -0.3em;
+        fill: currentColor;
+        overflow: hidden;
+        vertical-align: middle; 
+    }
+</style>
