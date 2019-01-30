bd0d5833e74cecd9148841979ee0fcef9cad6446
dinglindong <258231674@qq.com>
2019-01-30 15:25:22 +0800

    issue：element describe：add wrap ele

--- src/views/clipboard/index.vue
+++ src/views/clipboard/index.vue
@@ -1,14 +1,16 @@
 <template>
-    <el-tabs v-model="activeName">
-        <el-tab-pane :label="$t('clipboard.tipsone')" name="directly">
-            <el-input v-model="inputData" placeholder="Please input" style="width:400px;max-width:100%;"/>
-            <el-button type="primary" icon="el-icon-document" @click="handleCopy(inputData,$event)">copy</el-button>
-        </el-tab-pane>
-        <el-tab-pane :label="$t('clipboard.tipstwo')" name="v-directive">
-            <el-input v-model="inputData" placeholder="Please input" style="width:400px;max-width:100%;"/>
-            <el-button v-clipboard:copy="inputData" v-clipboard:success="clipboardSuccess" type="primary" icon="el-icon-document">copy</el-button>
-        </el-tab-pane>
-    </el-tabs>
+    <section class="app-container">
+        <el-tabs v-model="activeName">
+            <el-tab-pane :label="$t('clipboard.tipsone')" name="directly">
+                <el-input v-model="inputData" placeholder="Please input" style="width:400px;max-width:100%;"/>
+                <el-button type="primary" icon="el-icon-document" @click="handleCopy(inputData,$event)">copy</el-button>
+            </el-tab-pane>
+            <el-tab-pane :label="$t('clipboard.tipstwo')" name="v-directive">
+                <el-input v-model="inputData" placeholder="Please input" style="width:400px;max-width:100%;"/>
+                <el-button v-clipboard:copy="inputData" v-clipboard:success="clipboardSuccess" type="primary" icon="el-icon-document">copy</el-button>
+            </el-tab-pane>
+        </el-tabs>
+    </section>
 </template>
 
 <script>
