82ea8bab11bd35fc97ff080d4f04498ab3f44acc
dinglindong <258231674@qq.com>
2019-02-19 15:28:36 +0800

    issue：draggable describe：add style annotation

--- src/components/Kanban/index.vue
+++ src/components/Kanban/index.vue
@@ -6,7 +6,9 @@
         <draggable
             class="board-column-content"
             :list="list"
-            :options="options">
+            :options="options"
+            :move="getdata"
+            @update="datadragEnd">
             <div v-for="element in list" :key="element.id" class="board-item">
                 {{element.name}} - {{element.id}}
             </div>
@@ -39,10 +41,67 @@
         },
         components: {
             draggable
+        },
+        methods: {
+            getdata (evt) {
+                console.log(evt.draggedContext.element.id)
+            },
+            datadragEnd (evt) {
+                console.log('拖动前的索引 :' + evt.oldIndex)
+                console.log('拖动后的索引 :' + evt.newIndex)
+                console.log(this.tags)
+            }
         }
     }
 </script>
 
 <style rel="stylesheet/scss" lang="scss">
 
+</style>
+<style lang="scss">
+    .board-column {
+        min-width: 300px;
+        min-height: 100px;
+        height: auto;
+        overflow: hidden;
+        background: #f0f0f0;
+        border-radius: 3px;
+        /*border:1px solid #f00;*/
+
+        .board-column-header {
+            height: 50px;
+            line-height: 50px;
+            overflow: hidden;
+            padding: 0 20px;
+            text-align: center;
+            background: #333;
+            color: #fff;
+            /*border:1px solid #f00;*/
+        }
+
+        .board-column-content {
+            height: auto;
+            overflow: hidden;
+            border: 10px solid transparent;
+            min-height: 60px;
+            display: flex;
+            justify-content: flex-start;
+            flex-direction: column;
+            align-items: center;
+            /*border:1px solid #f00;*/
+
+            .board-item {
+                cursor: pointer;
+                width: 100%;
+                height: 64px;
+                margin: 5px 0;
+                background-color: #fff;
+                text-align: left;
+                line-height: 54px;
+                padding: 5px 10px;
+                box-sizing: border-box;
+                box-shadow: 0px 1px 3px 0 rgba(0,0,0,0.2);
+            }
+        }
+    }
 </style>
--- src/views/components-demo/dragKanban.vue
+++ src/views/components-demo/dragKanban.vue
@@ -1,8 +1,26 @@
 <template>
-    <section>
-        <Kanban :key="1" :list="list1" :options="options" class="kanban todo" header-text="Todo"></Kanban>
-        <Kanban :key="2" :list="list2" :options="options" class="kanban working" header-text="Working"></Kanban>
-        <Kanban :key="3" :list="list3" :options="options" class="kanban done" header-text="Done"></Kanban>
+    <section class="components-container">
+        <section class="board">
+            <Kanban :key="1" :list="list1" :options="options" class="kanban todo" header-text="Todo"></Kanban>
+            <Kanban :key="2" :list="list2" :options="options" class="kanban working" header-text="Working"></Kanban>
+            <Kanban :key="3" :list="list3" :options="options" class="kanban done" header-text="Done"></Kanban>
+        </section>
+        <div class="content board">
+            <ul>
+                <h3>list1</h3>
+                <li v-for="ele in list1">{{ele.name}}-{{ele.id}}</li>
+            </ul>
+
+            <ul>
+                <h3>list2</h3>
+                <li v-for="ele in list2">{{ele.name}}-{{ele.id}}</li>
+            </ul>
+
+            <ul>
+                <h3>list3</h3>
+                <li v-for="ele in list3">{{ele.name}}-{{ele.id}}</li>
+            </ul>
+        </div>
     </section>
 </template>
 
@@ -17,8 +35,10 @@
         data(){
             return {
                 options:{
-                    sort:true,
-                    animation:10
+                    sort:true,         //是否可以组内拖拽排序
+                    group: 'mission',   //是否可以跨组拖拽
+                    // delay:10,
+                    // animation:10,
                 },
                 list1:[
                     {name:'Mission',id:1},
@@ -37,10 +57,47 @@
                     {name:'Mission',id:10}
                 ]
             }
+        },
+        methods:{
+            handleClick(){
+                // 可以实时获取当前数组的值
+                console.log(this.list1)
+                console.log(this.list2)
+                console.log(this.list3)
+            }
         }
     }
 </script>
 
 <style rel="stylesheet/scss" lang="scss" scoped>
+    // flex布局分三栏
+    .board {
+        display: flex;
+        justify-content: space-around;
+        flex-direction: row;
+        align-items: flex-start;
+    }
+</style>
 
+<style rel="stylesheet/scss" lang="scss">
+    // 三栏头部颜色区分，scoped子组件无法继承
+    .board{
+        .kanban {
+            &.todo {
+                .board-column-header {
+                    background: #4A9FF9;
+                }
+            }
+            &.working {
+                .board-column-header {
+                    background: #f9944a;
+                }
+            }
+            &.done {
+                .board-column-header {
+                    background: #2ac06d;
+                }
+            }
+        }
+    }
 </style>
