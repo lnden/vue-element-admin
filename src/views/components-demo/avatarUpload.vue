c3d2e44ff9ce42cd9a6c3de7ee7fcc3809bbf768
dinglindong <258231674@qq.com>
2019-02-22 16:46:25 +0800

    issue：avatarUpload describe：add module

--- src/views/components-demo/avatarUpload.vue
+++ src/views/components-demo/avatarUpload.vue
@@ -1,15 +1,54 @@
 <template>
-    <section>
-        avatarUpload
+    <section class="components-container">
+        <code>This is based on
+            <a class="link-type" href="//github.com/dai-siki/vue-image-crop-upload"> vue-image-crop-upload</a>.
+            <p>{{ $t('components.imageUploadTips') }}</p>
+        </code>
+        <pan-thumb :image="image"/>
+
+        <el-button type="primary" icon="upload" style="position: absolute;bottom: 15px;margin-left: 40px;" @click="imagecropperShow=true">Change Avatar</el-button>
+
+        <image-cropper
+            v-show="imagecropperShow"
+            :width="300"
+            :height="300"
+            :key="imagecropperKey"
+            url="https://httpbin.org/post"
+            lang-type="en"
+            @close="close"
+            @crop-upload-success="cropSuccess"/>
     </section>
 </template>
 
 <script>
+    import PanThumb from '@/components/PanThumb'
+    import ImageCropper from '@/components/ImageCropper'
     export default {
-        name:'avatarUpload'
+        name:'avatarUpload',
+        components: {
+            PanThumb,
+            ImageCropper
+        },
+        data() {
+            return {
+                imagecropperShow: false,
+                imagecropperKey: 0,
+                image: 'https://wpimg.wallstcn.com/577965b9-bb9e-4e02-9f0c-095b41417191'
+            }
+        },
+        methods: {
+            cropSuccess(resData) {
+                this.imagecropperShow = false
+                this.imagecropperKey = this.imagecropperKey + 1
+                this.image = resData.files.avatar
+            },
+            close() {
+                this.imagecropperShow = false
+            }
+        }
     }
 </script>
 
 <style ref="stylesheet" lang="scss" scoped>
-    
+
 </style>
