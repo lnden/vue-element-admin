f36aee1bb35ef83c1b12062c95eb250d9912f329
dinglindong <258231674@qq.com>
2019-03-26 17:34:41 +0800

    add table loading

--- src/views/table/complexTable.vue
+++ src/views/table/complexTable.vue
@@ -32,48 +32,57 @@
             highlight-current-row
             style="width: 100%;"
             @sort-change="sortChange">
+
             <el-table-column :label="$t('table.id')" prop="id" sortable="custom" align="center" width="65">
                 <template slot-scope="scope">
                     <span>{{ scope.row.id }}</span>
                 </template>
             </el-table-column>
+
             <el-table-column :label="$t('table.date')" width="150px" align="center">
                 <template slot-scope="scope">
                     <span>{{ scope.row.timestamp | parseTime('{y}-{m}-{d} {h}:{i}') }}</span>
                 </template>
             </el-table-column>
+
             <el-table-column :label="$t('table.title')" min-width="150px">
                 <template slot-scope="scope">
                     <span class="link-type" @click="handleUpdate(scope.row)">{{ scope.row.title }}</span>
                     <el-tag>{{ scope.row.type | typeFilter }}</el-tag>
                 </template>
             </el-table-column>
+
             <el-table-column :label="$t('table.author')" width="110px" align="center">
                 <template slot-scope="scope">
                     <span>{{ scope.row.author }}</span>
                 </template>
             </el-table-column>
+
             <el-table-column v-if="showReviewer" :label="$t('table.reviewer')" width="110px" align="center">
                 <template slot-scope="scope">
                     <span style="color:red;">{{ scope.row.reviewer }}</span>
                 </template>
             </el-table-column>
+
             <el-table-column :label="$t('table.importance')" width="80px">
                 <template slot-scope="scope">
-                    <svg-icon v-for="n in +scope.row.importance" :key="n" icon-class="star" class="meta-item__icon"/>
+                    <svg-icon v-for="n in +scope.row.importance" :key="n" icon-class="table-star" class="meta-item__icon"/>
                 </template>
             </el-table-column>
+
             <el-table-column :label="$t('table.readings')" align="center" width="95">
                 <template slot-scope="scope">
                     <span v-if="scope.row.pageviews" class="link-type" @click="handleFetchPv(scope.row.pageviews)">{{ scope.row.pageviews }}</span>
                     <span v-else>0</span>
                 </template>
             </el-table-column>
+
             <el-table-column :label="$t('table.status')" class-name="status-col" width="100">
                 <template slot-scope="scope">
                     <el-tag :type="scope.row.status | statusFilter">{{ scope.row.status }}</el-tag>
                 </template>
             </el-table-column>
+
             <el-table-column :label="$t('table.actions')" align="center" width="230" class-name="small-padding fixed-width">
                 <template slot-scope="scope">
                     <el-button type="primary" size="mini" @click="handleUpdate(scope.row)">{{ $t('table.edit') }}</el-button>
@@ -85,6 +94,7 @@
                     </el-button>
                 </template>
             </el-table-column>
+
         </el-table>
 
         <!--<pagination v-show="total>0" :total="total" :page.sync="listQuery.page" :limit.sync="listQuery.limit" @pagination="getList" />-->
