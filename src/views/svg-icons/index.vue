b0f80b692c52ff38f2b78b4f1e588d215e376eca
dinglindong <258231674@qq.com>
2019-04-10 15:35:46 +0800

    ADD ELEMENT-UI SVG

--- src/views/svg-icons/index.vue
+++ src/views/svg-icons/index.vue
@@ -3,34 +3,58 @@
         <p class="warn-content">
             <a href="https://www.iconfont.cn" target="_blank">Add and use</a>
         </p>
-        <div class="icons-wrapper">
-            <el-tooltip placement="top" v-for="item of iconsMap" :key="item">
-                <div slot="content">
-                    {{ generateIconCode(item) }}
+        <el-tabs type="border-card">
+            <el-tab-pane label="Icons">
+                <div class="icons-wrapper">
+                    <el-tooltip placement="top" v-for="item of iconsMap" :key="item">
+                        <div slot="content">
+                            {{ generateIconCode(item) }}
+                        </div>
+                        <div class="icon-item" @click="handleClipboard(generateIconCode(item),$event)">
+                            <svg-icon :icon-class="item" class-name="disabled" />
+                            <span>{{ item }}</span>
+                        </div>
+                    </el-tooltip>
                 </div>
-                <div class="icon-item" @click="handleClipboard(generateIconCode(item),$event)">
-                    <svg-icon :icon-class="item" class-name="disabled" />
-                    <span>{{ item }}</span>
+            </el-tab-pane>
+
+            <el-tab-pane label="Element-UI Icons">
+                <div class="icons-wrapper">
+                    <el-tooltip placement="top" v-for="item of elementIcons" :key="item">
+                        <div slot="content">
+                            {{ generateElementIconCode(item) }}
+                        </div>
+                        <div class="icon-item" @click="handleClipboard(generateElementIconCode(item),$event)">
+                            <i :class="'el-icon-' + item" />
+                            <span>{{ item }}</span>
+                        </div>
+                    </el-tooltip>
                 </div>
-            </el-tooltip>
-        </div>
+            </el-tab-pane>
+        </el-tabs>
+
     </section>
 </template>
 
 <script>
     import icons from './requireIcons'
     import clipboard from '@/utils/clipboard'
+    import elementIcons from './element-icon.json'
 
     export default {
         name: 'Icons',
         data() {
             return {
-                iconsMap: icons
+                iconsMap: icons,
+                elementIcons: elementIcons
             }
         },
         methods: {
             generateIconCode(symbol) {
                 return `<svg-icon icon-class="${symbol}" />`
+            },
+            generateElementIconCode(symbol) {
+                return `<i class="el-icon-${symbol}" />`
             },
             handleClipboard(text, event) {
                 clipboard(text, event)
