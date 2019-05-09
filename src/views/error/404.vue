c2f848b96fe90d46a4dc9331dcc354514b2c9905
karte <258231674@qq.com>
2019-05-10 03:24:24 +0800

    Update 404.vue

--- src/views/error/404.vue
+++ src/views/error/404.vue
@@ -9,7 +9,7 @@
         <div class="bullshit">
             <div class="bullshit__oops">OOPS!</div>
             <div class="bullshit__info">版权所有
-                <a class='link-type' href='https://dinglindong.github.io/' target='_blank'>Lindong Ding</a>
+                <a class='link-type' href='https://lnden.github.io/' target='_blank'>Lnden</a>
             </div>
             <div class="bullshit__headline">{{ message }}</div>
             <div class="bullshit__info">请检查您输入的网址是否正确，请点击以下按钮返回主页或者发送错误报告</div>
